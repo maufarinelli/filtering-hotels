7bea42884857190a7b1cd77754504f3d5cfe262f
Farinelli, Mauricio <Mauricio.Farinelli@Kronos.com>
2017-02-24 17:16:23 -0500

    incluing files

---
+++ app/prices/prices.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
+
+import { FilterState } from '../state/app.initialState';
+
+import { FilteringHotelsService } from '../filtering-hotels.service';
+import { Price } from './price';
+
+@Component({
+  moduleId: module.id,
+  selector: 'prices',
+  templateUrl: './prices.html'
+})
+export class PricesComponent {
+  priceValues: Price[];
+  selectedStartPrice: number;
+  selectedEndPrice: number;
+
+  constructor(
+    private filteringHotelsService: FilteringHotelsService,
+    private ngRedux: NgRedux<FilterState>
+  ) {}
+
+  ngOnInit(): void {
+    this.filteringHotelsService.getPriceValues()
+      .then(priceValues => this.priceValues = priceValues);
+  }
+
+  onSelectStartPrice() {
+    this.selectedStartPrice = this.selectedStartPrice;
+    this.ngRedux.dispatch({ type: 'PRICE_START_CHANGE', payload: {id: 100, name: 'startPrice', value: this.selectedStartPrice} });
+  }
+
+  onSelectEndPrice() {
+    this.selectedEndPrice = this.selectedEndPrice;
+    this.ngRedux.dispatch({ type: 'PRICE_END_CHANGE', payload: {id: 101, name: 'endPrice', value: this.selectedEndPrice} });
+  }
+
+  startPriceState(price) {
+    return this.selectedEndPrice <= price.value;
+  }
+
+  endPriceState(price) {
+    return this.selectedStartPrice >= price.value;
+  }
+}
