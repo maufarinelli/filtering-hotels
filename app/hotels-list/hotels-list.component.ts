da10b5f487c30c9c96240e6bcfaea20c78c537e5
Mauricio Farinelli <Mauricio Farinelli>
2017-03-05 20:26:27 -0500

    end of filtering, based on state

--- app/hotels-list/hotels-list.component.ts
+++ app/hotels-list/hotels-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgRedux } from 'ng2-redux';
-import { Observable } from 'rxjs/Observable';
-import * as _ from "lodash";
+import * as _ from 'lodash';
 
 import { FilterState } from '../state/app.initialState';
 
@@ -14,8 +13,8 @@ import { FilteringHotelsService } from '../filtering-hotels.service';
 })
 export class HotelsListComponent implements OnInit {
   private hotels: Object[];
-  private allHotels: Object[];
-  private accommodationTypesState$
+  private allHotels;
+  private accommodationTypesState$;
 
   constructor(
     private filteringHotelsService: FilteringHotelsService,
@@ -35,10 +34,16 @@ export class HotelsListComponent implements OnInit {
   }
 
   onFilterHotels(state) {
-    var selectedAccommodations = state.accommodationTypes.filter(accommodationType => accommodationType.checked);
+    var selectedAccommodations = state.accommodationTypes.filter(accommodationType => accommodationType.checked),
+      startPrice = _.find(state.prices, { name: 'startPrice' }),
+      endPrice = _.find(state.prices, { name: 'endPrice' }),
+      startScore = _.find(state.scores, { name: 'startScore' }),
+      endScore = _.find(state.scores, { name: 'endScore' });
 
     this.hotels = this.allHotels.filter(hotel => {
-      return !!_.find(selectedAccommodations, {name: hotel.accommodationType});
+      return !!_.find(selectedAccommodations, {name: hotel.accommodationType}) &&
+        parseFloat(hotel.price) >= startPrice.value && parseFloat(hotel.price) <= endPrice.value &&
+        parseFloat(hotel.score) >= startScore.value && parseFloat(hotel.score) <= endScore.value;
     });
   }
 }
